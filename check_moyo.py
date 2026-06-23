725483ab5247fa4ee1bd49ec6d866be876782433
manshujaeng <shinwc38@gmail.com>
2026-06-23 13:25:52 +0900

    Update check_moyo.py

--- check_moyo.py
+++ check_moyo.py
@@ -21,8 +21,8 @@ url = (
 
 KNOWN_FILE = "known_moyo.json"
 
-BOT_TOKEN = os.environ["TELEGRAM_TOKEN"]
-CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
 
 def send_telegram(message: str):
